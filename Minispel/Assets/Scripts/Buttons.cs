e64b6cd46b414dad485406c9d925c3a763cfb0b3
K_F07 <kalle.ehrsson@elev.ga.lbs.se>
2024-12-12 18:52:39 +0100

    quit knapp

--- Minispel/Assets/Scripts/Buttons.cs
+++ Minispel/Assets/Scripts/Buttons.cs
@@ -8,14 +8,36 @@ public class Buttons : MonoBehaviour
 {
     public DeathScore deathScore;
 
-    public void RestartGameButton()
+    public void Restart()
     {
         SceneManager.LoadScene("MainScene");
     }
+
 
     public void ResetHighscore()
     {
         PlayerPrefs.SetInt("HighScore", 0);
         deathScore.Start();
     }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public interface IQuitGame
+    {
+        void OnQuit();
+    }
+
+    public interface IResetHighscore
+    {
+        void ResetHighscore();
+    }
+
+    public interface IRestartGame
+    {
+        void Restart();
+    }
+
 }
